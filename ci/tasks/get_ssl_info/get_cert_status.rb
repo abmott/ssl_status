ca8d5e04a4a7918fad3619a235ad6257b215af9f
abmott <adam.mott@csaa.com>
2018-06-14 09:53:38 -0700

    testing array

--- ci/tasks/get_ssl_info/get_cert_status.rb
+++ ci/tasks/get_ssl_info/get_cert_status.rb
@@ -81,5 +81,6 @@ datadogoutput = `curl -sS -H "Content-type: application/json" -X POST -d \
 File.delete("certcheck.cer") if File.exist?("certcheck.cer")
 File.delete("#{file_name}") if File.exist?("#{file_name}")
 #puts datadogoutput
-array_test = ["#{ENV['test_array']}"]
+array_test = %w("#{ENV['TEST_ARRAY']}")
+puts array_test
 array_test.each do { |name| puts name }
